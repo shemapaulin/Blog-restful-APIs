5065b794bf7a494dd16d702c41aba4479102ba4c
shema paulin <shema@post.com>
2023-11-24 16:42:30 +0200

    crud feature of Reply

--- src/Routes/replyRoutes.js
+++ src/Routes/replyRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
-import { postReply,getReply } from "../Controllers/reply.js";
+import { postReply,getReply, updateReply, deleteReply } from "../Controllers/reply.js";
 
 const router=Router();
 
 router.post("/addReply",postReply);
-router.get("/getReply",getReply)
+router.get("/getReply/:id",getReply);
+router.put("/updateReply/:id",updateReply);
+router.delete("/deleteReply/:id",deleteReply);
 
 
 const RepRouter=router;
